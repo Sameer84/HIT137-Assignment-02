8761db3619e2c433e6a34e7027d41679ee208f97
Susanti Djie <susanti88@naver.com>
2024-01-08 09:28:29 +1100

    q1 task1   - Read 4 csv files   - Create single .txt file   - Merged from CSV to .txt

--- q1/t1.py
+++ q1/t1.py
@@ -1,3 +1,13 @@
 # 1-1 Extract file
 # Extract the ‘text’ in all the CSV files
 # and store them into a single ‘.txt file’.
+
+csv_files = ['./q1/CSV1.csv', './q1/CSV2.csv', './q1/CSV3.csv', './q1/CSV4.csv']
+with open('./q1/merged_csv.txt', 'w') as f_out:
+    for file in csv_files:
+        with open(file, 'r') as f_in:
+            content = f_in.read()
+            f_out.write(content)
+
+f_in.close()
+f_out.close()
