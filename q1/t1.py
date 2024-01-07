339b2aaa9b3c069e39307044174ce33e19f4bdd2
Susanti Djie <susanti88@naver.com>
2024-01-08 09:49:45 +1100

    add exeption handling  - use try except finally

--- q1/t1.py
+++ q1/t1.py
@@ -2,12 +2,17 @@
 # Extract the ‘text’ in all the CSV files
 # and store them into a single ‘.txt file’.
 
-csv_files = ['./q1/CSV1.csv', './q1/CSV2.csv', './q1/CSV3.csv', './q1/CSV4.csv']
-with open('./q1/merged_csv.txt', 'w') as f_out:
-    for file in csv_files:
-        with open(file, 'r') as f_in:
-            content = f_in.read()
-            f_out.write(content)
-
-f_in.close()
-f_out.close()
+try:
+    csv_files = ['./q1/CSV1.csv', './q1/CSV2.csv', './q1/CSV3.csv', './q1/CSV4.csv']
+    with open('./q1/merged_csv.txt', 'w') as f_out:
+        for file in csv_files:
+            with open(file, 'r') as f_in:
+                content = f_in.read()
+                f_out.write(content)
+    print('proccess done.')
+except IOError as e:
+    print('IOError : ', e.errno, e)
+finally:
+    print('clean up...')
+    f_in.close()
+    f_out.close()
