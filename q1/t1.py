b4bea331a46181c805c5f804a7309e1563e8c46e
Susanti Djie <susanti88@naver.com>
2024-01-08 19:30:07 +1100

    added instruction task1

--- q1/t1.py
+++ q1/t1.py
@@ -3,14 +3,17 @@
 # and store them into a single ‘.txt file’.
 
 try:
+    # put csv file names to list
     csv_files = ['./q1/CSV1.csv', './q1/CSV2.csv', './q1/CSV3.csv', './q1/CSV4.csv']
     with open('./q1/merged_csv.txt', 'w') as f_out:
+        # open each csv
         for file in csv_files:
             with open(file, 'r') as f_in:
                 content = f_in.read()
                 f_out.write(content)
     print('proccess done.')
 except IOError as e:
+    # if any csv file is missing
     print('IOError : ', e.errno, e)
 finally:
     print('clean up...')
