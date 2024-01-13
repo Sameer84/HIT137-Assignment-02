ebd01a5b0eb55d604219ff85f6b77319f0c97c84
nishat199 <nishatanjudhaka18@gmail.com>
2024-01-14 00:16:51 +0600

    final correction

--- q3/final_correction.py
+++ q3/final_correction.py
@@ -1,39 +1,41 @@
-global_variable = 100 #global_variable is set to 100
-my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'} # my_dic is initialized with some key-values
+global_variable = 100
+my_dict = {'key1': 'value1', 'ke12': 'value2', 'ke13': 'value3'}
 
-def process_numbers(numbers): #function process_numbers take parameter of numbers
-    global global_variable 
-    local_variable = 5
-    numbers = [1, 2, 3, 4, 5] #local variable numbers is initialized to [,2,3,4,5]
-
-    while local_variable > 0: #while loop is used to iterate local varible is greater than 0
-        if local_variable % 2 == 0: #if it is even numbers
-            numbers.remove(local_variable) #removes number from list
-        local_variable -= 1
-    return numbers #returns modified numbers
-
-my_set = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1} #my_set is inialized
-result = process_numbers(numbers=my_set)
-
-def modify_dict(local_variable ): # function modify_dict takes parameter local_variable
-    my_dict['key4'] = local_variable #Adds a new key to my_dict whith the value of local_variable
-
-modify_dict(10) 
-
-def upbate_global():
+def process_numbers():
     global global_variable
-    global_variable += 10 #increaments by 10
+    local_variable = 5
+    numbers = [1, 2, 3, 4, 5]
 
-for i in range(5):#Iterates over the range 0 to 4
+    while local_variable > 0:
+        if local_variable % 2 == 0:
+            numbers.remove(local_variable)
+        local_variable -= 1
+    return numbers
+
+my_set = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
+result = process_numbers()  # Corrected: Removed unnecessary argument from process_numbers()
+
+def modify_dict():
+    local_variable = 10
+    my_dict['ke14'] = local_variable
+
+modify_dict()  # Corrected: Removed unnecessary argument from modify_dict()
+
+def update_global():
+    global global_variable
+    global_variable += 10
+
+for i in range(5):
     print(i)
-    i += 1
+    # Corrected: Changed variable I to lowercase i in loop
+    # Removed unnecessary I += 1 line
 
-if my_set is not None and my_dict['key4'] == 10:
+if my_set is not None and my_dict['ke14'] == 10:
     print("Condition met!")
 
-if 5 not in my_dict:
+if 5 not in my_dict: # Corrected: Changed my_set to my_dict in conditional. Since checking if 5 is in dictionary, not set
     print("5 not found in the dictionary!")
 
-print(global_variable) #print value of global_variable
-print(my_dict)#print value of my_dict
-print(my_set)#print value of my_set
+print(global_variable)
+print(my_dict)
+print(my_set)
